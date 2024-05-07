fe92eb4e4d0abef605fe8da2e720e52290f8488a
mdasif8393 <md.asif.8393@gmail.com>
2024-05-07 15:26:44 +0600

    add react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ProductList />
     </>
   );
 }
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const retrieveProducts = async () => {
+  const response = await axios.get("http://localhost:3000/products");
+  return response.data;
+};
+
+export default function ProductList() {
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: retrieveProducts,
+  });
+
+  if (isLoading) return <div>fetching products...</div>;
+
+  if (error) return <div>An error occured: {error.message}</div>;
+
+  return (
+    <div className="flex flex-col justify-center items-center w-3/5">
+      <h2 className="text-3xl my-2">Product List</h2>
+      <ul>
+        {products &&
+          products.map((product) => (
+            <li key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <p>{product.title}</p>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
