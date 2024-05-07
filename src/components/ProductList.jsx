d5b28c3017d2e20114b9dfe0d4e827285f99c97c
mdasif8393 <md.asif.8393@gmail.com>
2024-05-07 18:06:25 +0600

    add devtool

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -23,12 +23,19 @@ export default function ProductList() {
   return (
     <div className="flex flex-col justify-center items-center w-3/5">
       <h2 className="text-3xl my-2">Product List</h2>
-      <ul>
+      <ul className="flex flex-wrap justify-center items-center">
         {products &&
           products.map((product) => (
-            <li key={product.id}>
-              <img src={product.thumbnail} alt={product.title} />
-              <p>{product.title}</p>
+            <li
+              key={product.id}
+              className="flex flex-col items-center m-2 border rounded-sm"
+            >
+              <img
+                className="object-cover h-64 w-96 rounded-sm"
+                src={product.thumbnail}
+                alt={product.title}
+              />
+              <p className="text-xl my-3">{product.title}</p>
             </li>
           ))}
       </ul>
